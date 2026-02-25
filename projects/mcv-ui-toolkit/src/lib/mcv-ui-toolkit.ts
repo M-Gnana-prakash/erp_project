b6f09eaf84f967fff57c988bacd68a6f034a83d4
arjunea-tech <arjunea@anjaconline.org>
2026-02-25 15:32:21 +0530

    Library folder created

---
+++ projects/mcv-ui-toolkit/src/lib/mcv-ui-toolkit.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mcv-mcv-ui-toolkit',
+  imports: [],
+  template: `
+    <p>
+      mcv-ui-toolkit works!
+    </p>
+  `,
+  styles: ``,
+})
+export class McvUiToolkit {
+
+}
